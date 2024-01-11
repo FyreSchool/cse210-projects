a259872138a1cad369c456e50b4e1732335b0eff
Spencer Wyman <wym22001@byui.edu>
2024-01-10 18:29:15 -0700

    lesson done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,19 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        //Askinf user for thier first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        //asking user for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+
+        //printing out their first and last name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
+
 }
