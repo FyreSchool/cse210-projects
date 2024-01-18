bef0f5fee05be3b2ed10ff7fb4ebcb5894f08041
Spencer Wyman <wym22001@byui.edu>
2024-01-17 17:02:52 -0700

    lesson done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+
+        //Create list for numbers
+        List<int> numbers = new List<int>();
+
+        int addNumber = -1;
+        int sum = 0;
+        int maxNumber = 0;
+
+        //Asking user to enter numbers into the list
+        Console.WriteLine("Please enter a list of number, type 0 when finished. ");
+
+        //Runs a loop that lets user add number to the list until they enter 0
+        while (addNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            String number = Console.ReadLine();
+
+            addNumber = int.Parse(number);
+
+            //makes it so 0 is not added to the list
+            if (addNumber != 0)
+            {
+                numbers.Add(addNumber);
+            }
+        }
+        
+        //Sums the numbers in list together and finds the biggest number
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+
+        //finds the average of the numbers.
+        int count = numbers.Count;
+        double average = (double)sum / count;
+
+        Console.WriteLine("Sum of numbers: " + sum);
+        Console.WriteLine("Average of numbers: " + average);
+        Console.WriteLine("The largest number is: " + maxNumber);
+
     }
 }
