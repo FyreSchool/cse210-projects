fc63d645fb0089a515884495fd07a54e3ca753c8
Spencer Wyman <wym22001@byui.edu>
2024-02-05 11:43:22 -0700

    Lesson Done

---
+++ prepare/Learning03/Fractions.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Fractions
+{
+    private int topFraction;
+    private int bottomFraction;
+
+    public Fractions()
+    {
+        topFraction = 1;
+        bottomFraction = 1;
+    }
+
+    public Fractions(int number)
+    {
+        topFraction = number;
+        bottomFraction = 1;
+    }
+
+    public Fractions(int top, int bottom)
+    {
+        topFraction = top;
+        bottomFraction = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string frac = $"{topFraction}/{bottomFraction}";
+        return frac;
+    }
+
+    public double GetDecimalValue()
+    {
+        double decimalFraction = (double)topFraction / (double)bottomFraction;
+        return decimalFraction;
+    }
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        //fraction with one on top and bottom
+        Fractions frac1 = new Fractions();
+        
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        //fraction of 3 divied by 4
+        Fractions frac2 = new Fractions(3,4);
+
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        //fraction of 18 divied by 4
+        Fractions frac3 = new Fractions(18,4);
+
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
+
+        //fraction of 3 divied by 1
+        Fractions frac4 = new Fractions(3);
+
+        Console.WriteLine(frac4.GetFractionString());
+        Console.WriteLine(frac4.GetDecimalValue());
+
     }
 }
+
