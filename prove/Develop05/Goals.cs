4d1bf2ed652aa35aeccc8471ef9b90fa6f48b238
Spencer Wyman <wym22001@byui.edu>
2024-03-11 09:24:03 -0600

    lesson in progress

---
+++ prove/Develop05/Goals.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Goal
+{
+    protected string goalName;
+    protected string goalDescription;
+
+
+    public Goal(string goalName, string goalDescription)
+    {
+        this.goalName = goalName;
+        this.goalDescription = goalDescription;
+    }
+} 
