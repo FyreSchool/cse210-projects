e0f419ed3f071de19a70a9d9b76e073d63340b6b
Spencer Wyman <wym22001@byui.edu>
2024-03-11 10:00:18 -0600

    lesson in progress

--- prove/Develop05/Goals.cs
+++ prove/Develop05/Goals.cs
@@ -4,11 +4,46 @@ public class Goal
 {
     protected string goalName;
     protected string goalDescription;
+    protected int goalPoints;
 
+    public List<string> goals = new List<string>();
 
     public Goal(string goalName, string goalDescription)
     {
         this.goalName = goalName;
         this.goalDescription = goalDescription;
     }
+
+    public string GetGoalName()
+    {
+        Console.WriteLine("What is the name of your goal? ");
+        goalName = Console.ReadLine();
+        return goalName;
+    }
+
+    public string GetGoalDescription()
+    {
+        Console.WriteLine("What is a short description of your goal? ");
+        goalDescription = Console.ReadLine();
+        return goalDescription;
+    }
+    public int GoalPoints()
+    {
+        Console.WriteLine("What is the amount of points associated with this goal? ");
+        goalPoints = Console.ReadLine();
+        return goalPoints;
+    }
+        public void AddToList()
+    {
+        //add entries to list
+        goals.Add();
+    }
+
+    public void CreateGoal()
+    {
+        GetGoalName();
+        GetGoalDescription();
+        GoalPoints();
+    }
 } 
+
