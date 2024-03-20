da7437e4e17d949dcce966a44b25e84f62e132e7
TannerWyman <wym22002@byui.edu>
2024-03-20 01:59:30 -0600

    done

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,15 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Activity> activities = new List<Activity>();
+
         Running run1 = new Running("05 Mar 2024", 45, 5);
         Biking bike1 = new Biking("07 Mar 2024", 60, 15);
         Swimming swim1 = new Swimming("09 Mar 2024", 30,25);
 
-        run1.GetSummary();
-        Console.WriteLine();
-        bike1.GetSummary();
-        Console.WriteLine();
-        swim1.GetSummary();
+        activities.Add(run1);
+        activities.Add(bike1);
+        activities.Add(swim1);
+
+        foreach (Activity i in activities)
+        {
+            i.GetSummary(); 
+            Console.WriteLine();   
+
+        }
 
     }
 }
